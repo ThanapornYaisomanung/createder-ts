e4ea255b19a1d19b470d434bde02dde9fecb561f
Poramee Suriyajanno <pookoak8501@gmail.com>
2023-04-10 17:58:26 +0700

    update code page and Navbar80%

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,6 @@ import Menu from "@/components/Menu"
 
 export default function Home() {
   return (
-<<<<<<< Updated upstream
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>สวัสดีค่ะ dev2</p>
-    </main>
-=======
     <div>
       <Menu/>
       <div className="flex min-h-screen flex-col items-center justify-between p-24" style={{ fontFamily: 'Prompt' }}>
@@ -22,6 +17,5 @@ export default function Home() {
         </div>
       </div>
     </div>
->>>>>>> Stashed changes
   )
 }
