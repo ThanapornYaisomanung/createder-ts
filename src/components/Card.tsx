1858eff1749ed930bd6cdc5cc6496ea48f363040
Thanaporn <tanapornyaisomanung@gmail.com>
2023-04-11 13:47:10 +0700

    add footter

---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+export default function CardList() {
+    return(
+        <>
+        <div className="w-80 bg-white h-[362px]">
+            <div className="img p-4 bg-slate-600">
+            </div>
+        </div>
+        </>
+    )
+}
